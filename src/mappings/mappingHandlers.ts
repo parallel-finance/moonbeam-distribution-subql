88c57f3710d89f923c84cb6321d8e8c1d2fefc6c
Yang Zhou <deedecho@gmail.com>
2022-01-16 16:04:06 +0800

    fix the wrong totalClaimed calculation

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,8 +3,8 @@ import { SpecVersion, EvmTransaction, TotalClaimed } from "../types";
 import MoonbeamDatasourcePlugin, { MoonbeamCall } from "@subql/contract-processors/dist/moonbeam";
 import { inputToFunctionSighash, isZero, wrapExtrinsics } from "../utils";
 
-const MAIN_REWARDS_ADDRESS = '0x3F5757Af3A9fCd2aA09856bbcC9038856aF6f8Bf';
-const DISTRIBUTION_ADDRESS = '0xD65Fee206a4ea89eBBcF4694E745C597AB6F8325';
+const MAIN_REWARDS_ADDRESS = '0x3f5757af3a9fcd2aa09856bbcc9038856af6f8bf';
+const DISTRIBUTION_ADDRESS = '0x51d16189a7cd896f6e4a8b264887d0f6f4d2b0eb';
 
 let specVersion: SpecVersion;
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
@@ -18,16 +18,16 @@ export async function handleBlock(block: SubstrateBlock): Promise<void> {
         await specVersion.save();
     }
     const evmCalls: MoonbeamCall[] = await Promise.all(wrapExtrinsics(block).filter(ext => ext.extrinsic.method.section === 'ethereum' && ext.extrinsic.method.method === 'transact').map((ext) => MoonbeamDatasourcePlugin.handlerProcessors['substrate/MoonbeamCall'].transformer(ext, {} as any, undefined, undefined))) as any;
-    evmCalls.forEach(async (call, idx) => 
-        await handleEvmTransaction(`${block.block.header.number.toString()}-${idx}`, call)
-    ); 
+    for (let idx = 0; idx < evmCalls.length; idx++) {
+        await handleEvmTransaction(`${block.block.header.number.toString()}-${idx}`, evmCalls[idx]);
+    }
 }
 
 export async function handleEvmTransaction(idx: string, tx: MoonbeamCall): Promise<void> {
     if (!tx.hash || !tx.success) {
         return;
     }
-    if (tx.from !== MAIN_REWARDS_ADDRESS || tx.to !== DISTRIBUTION_ADDRESS) {
+    if (tx.from != MAIN_REWARDS_ADDRESS || tx.to != DISTRIBUTION_ADDRESS) {
         return;
     }
     const func = isZero(tx.data) ? undefined : inputToFunctionSighash(tx.data);
@@ -36,23 +36,28 @@ export async function handleEvmTransaction(idx: string, tx: MoonbeamCall): Promi
         txHash: tx.hash,
         from: tx.from,
         to: tx.to,
+        value: tx.value.toString(),
         func,
-        blockHeight: BigInt(tx.blockNumber.toString()),
+        blockHeight: tx.blockNumber,
         success: tx.success,
     });
-    await evmTransaction.save();
+    logger.info(`evmTransaction: ${JSON.stringify(evmTransaction)}`);
 
     let totalClaimed = await TotalClaimed.get(DISTRIBUTION_ADDRESS);
     if (totalClaimed) {
-        totalClaimed.blockHeight = BigInt(tx.blockNumber.toString());
-        totalClaimed.amount = (BigInt(totalClaimed.amount) + BigInt(tx.value.toString())).toString(); 
+        totalClaimed.blockHeight = tx.blockNumber;
+        totalClaimed.amount = (BigInt(totalClaimed.amount) + BigInt(tx.value.toString())).toString();
     } else {
         totalClaimed = TotalClaimed.create({
             id: DISTRIBUTION_ADDRESS,
-            blockHeight: BigInt(tx.blockNumber.toString()),
+            blockHeight: tx.blockNumber,
             amount: tx.value.toString(),
-        })
+        });
     }
-    logger.info(JSON.stringify(totalClaimed));
-    await totalClaimed.save();
+    logger.info(`totalClaimed: ${JSON.stringify(totalClaimed)}`);
+
+    await Promise.all([
+        evmTransaction.save(),
+        totalClaimed.save(),
+    ]);
 }
